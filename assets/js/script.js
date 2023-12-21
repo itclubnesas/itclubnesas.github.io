eef4d4044c6fddc2d50b56d85d7e0bc00461f55c
Amelinda <111961908+Amelinda2705@users.noreply.github.com>
2023-12-21 07:35:49 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,26 @@ modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
 });
 
-// send message function
+//Fungsi mengirim pesan
 function send() {
   Email.send({
-    SecureToken: "f30c75e7-8644-442e-91d3-b83d4c311057",
-    To: "daffahmadibrahim@gmail.com",
-    From: "daffabot@programer.net",
-    Subject: "New Contact Form Enquiry",
-    Body:
-      "Name: " +
-      document.getElementById("name").value +
-      "<br> Email: " +
-      document.getElementById("email").value +
-      "<br> Message: " +
-      document.getElementById("message").value,
-  })
-    .then(function (message) {
-      if (message == "OK") {
-        alert("Message Succesfully Sent!");
-      } else {
-        alert("Something wrong i can feel it");
-      }
-    })
-    .catch((error) => {
-      alert("Error Sending Message: " + error);
-    });
+    SecureToken : "bcfb5f26-8616-41c7-9fe2-5c37edf0b2c4",
+    To : 'daffahmadibrahim@gmail.com',
+    From : 'daffabot@programmer.net',
+    Subject : "New Contact Form Enquiry",
+    Body : "Name: " + document.getElementById("name").value
+       + "<br> Email: " + document.getElementById("email").value
+       + "<br> Message: " + document.getElementById("message").value
+})
+.then(function (message) {
+  if (message == "OK") {
+    alert("Message Succesfully Sent!");
+  } else {
+    alert(message);
+  }
+})
+.catch((error) => {
+  console.log(error);
+  alert("Error Sending Message: " + error);
+});
 }
