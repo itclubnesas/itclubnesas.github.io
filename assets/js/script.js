7a89bb89496ae3a676f76eeb0e325e0171fac06c
Amelinda <amelindaa2705@gmail.com>
2023-12-21 07:48:38 +0700

    fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,36 @@ const body = document.querySelector("body"),
   modeSwitch = body.querySelector(".toggle-switch"),
   modeText = body.querySelector(".mode-text");
 
-// dark mode function
+  //Fungsi toggle dark
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
 });
 
-// send message function
+//Fungsi mengirim pesan
 function send() {
+  setTimeout(() => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  }, 200);
   Email.send({
-    SecureToken: "f30c75e7-8644-442e-91d3-b83d4c311057",
-    To: "daffahmadibrahim@gmail.com",
-    From: "daffabot@programer.net",
-    Subject: "New Contact Form Enquiry",
-    Body:
-      "Name: " +
-      document.getElementById("name").value +
-      "<br> Email: " +
-      document.getElementById("email").value +
-      "<br> Message: " +
-      document.getElementById("message").value,
-  })
-    .then(function (message) {
-      if (message == "OK") {
-        alert("Message Succesfully Sent!");
-      } else {
-        alert("Something wrong i can feel it");
-      }
-    })
-    .catch((error) => {
-      alert("Error Sending Message: " + error);
-    });
+    SecureToken : "bcfb5f26-8616-41c7-9fe2-5c37edf0b2c4",
+    To : 'daffahmadibrahim@gmail.com',
+    From : 'daffabot@programmer.net',
+    Subject : "New Contact Form Enquiry",
+    Body : "Name: " + document.getElementById("name").value
+       + "<br> Email: " + document.getElementById("email").value
+       + "<br> Message: " + document.getElementById("message").value
+})
+.then(function (message) {
+  if (message == "OK") {
+    alert("Message Succesfully Sent!");
+  } else {
+    alert(message);
+  }
+})
+.catch((error) => {
+  console.log(error);
+  alert("Error Sending Message: " + error);
+});
 }
