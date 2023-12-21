d36a8ec4b72a616e58ab7adce39343b80d520105
Amelinda <111961908+Amelinda2705@users.noreply.github.com>
2023-12-21 07:55:01 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@ const body = document.querySelector("body"),
   modeSwitch = body.querySelector(".toggle-switch"),
   modeText = body.querySelector(".mode-text");
 
-// dark mode function
+//function dark mode
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
 });
 
 //Fungsi mengirim pesan
 function send() {
+  setTimeout(() => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  }, 200);
   Email.send({
     SecureToken : "bcfb5f26-8616-41c7-9fe2-5c37edf0b2c4",
     To : 'daffahmadibrahim@gmail.com',
